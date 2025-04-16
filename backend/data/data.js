32e6d290a4bc311fcb474602d70cb27d1c4f7629
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-04-17 01:07:34 +0600

    simple data and diagrams added

---
+++ backend/data/data.js
@@ -0,0 +1,10 @@
+import { json } from "express";
+
+import Books from "./book.json";
+import Users from "./user.json";
+import Connections from "./connection.json";
+import Reviews from "./review.json";
+
+
+export { Books, Users, Connections, Reviews };
+
